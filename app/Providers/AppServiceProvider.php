4833630168b540a91f91779ca5d1b780644d6bf0
wubuze <wubuze@qq.com>
2018-06-28 18:00:08 +0800

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // MySQL default string length configuration
+        // for solving MySQL 5.7- migration issue
+        Schema::defaultStringLength(191);
     }
 
     /**
