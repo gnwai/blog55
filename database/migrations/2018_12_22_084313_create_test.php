16d247aff1b084437cd0c7bbcd30513bbf99cd00
wubuze <wubuze@qq.com>
2018-12-22 17:45:00 +0800

    123

---
+++ database/migrations/2018_12_22_084313_create_test.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTest extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->engine = 'innodb';
+
+            $table->increments('id');
+            $table->string('name',20);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
